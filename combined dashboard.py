209ee947e67b38e7cab40fa0838fee1fd2884129
jeniferXavier <jenifer.xavierraj@gmail.com>
2026-05-11 17:34:37 -0400

    Update demographics CSV file path in load_data function

--- combined dashboard.py
+++ combined dashboard.py
@@ -65,7 +65,7 @@ margin-bottom:25px;">
 @st.cache_data
 def load_data():
     df = pd.read_csv("cleaned_hupa_diabetes_recent1.xlsb")
-    demo = pd.read_csv("cleaned_demographics (1)(1).csv")
+    demo = pd.read_csv("cleaned_demographics.csv")
 
     df["time"] = pd.to_datetime(df["time"], errors="coerce")
 
