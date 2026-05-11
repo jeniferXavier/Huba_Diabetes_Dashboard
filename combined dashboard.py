7296ccc7b53b13f3387e31967b51a043d56eb145
jeniferXavier <jenifer.xavierraj@gmail.com>
2026-05-11 17:38:14 -0400

    Change data loading from CSV to Excel format

--- combined dashboard.py
+++ combined dashboard.py
@@ -64,7 +64,7 @@ margin-bottom:25px;">
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("cleaned_hupa_diabetes_recent1.xlsb")
+    df = pd.read_excel("cleaned_hupa_diabetes_recent1.xlsb")
     demo = pd.read_csv("cleaned_demographics.csv")
 
     df["time"] = pd.to_datetime(df["time"], errors="coerce")
