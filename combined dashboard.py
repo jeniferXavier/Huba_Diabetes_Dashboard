128de5233ace8ce360527d7f083e9c5ac3bb72af
jeniferXavier <jenifer.xavierraj@gmail.com>
2026-05-11 21:19:59 -0400

    Add files via upload

--- combined dashboard.py
+++ combined dashboard.py
@@ -45,7 +45,6 @@ st.markdown("""
     min-width: 320px;
     max-width: 320px;
     background: linear-gradient(180deg,#0B1F3A 0%, #102B50 100%);
-    margin:0px;
 }
 
 /* SIDEBAR BUTTON FULL WIDTH */
@@ -71,7 +70,10 @@ section[data-testid="stSidebar"] div.stButton > button:hover {
     transform: translateX(3px);
 }
 
-
+/* REMOVE EXTRA SPACE */
+section[data-testid="stSidebar"] {
+    padding-top: 5px;
+}
 
 
 """, unsafe_allow_html=True)
